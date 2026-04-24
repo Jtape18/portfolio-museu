765c771f0dbefac34d4b81f74ceedc71aa4f36f0
José Matias <josepaulo1607@gmail.com>
2026-04-24 16:56:50 -0300

    feat: ajustando comportamento de trocas de page e adicionando fotos novas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Footer from './components/museum/Footer'
 import Header from './components/museum/Header'
 import ActivityPage from './pages/ActivityPage'
 import DisciplinePage from './pages/DisciplinePage'
 import HomePage from './pages/HomePage'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [pathname])
+
+  return null
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="min-h-screen bg-[#090b10]">
         <Header />
         <main>
